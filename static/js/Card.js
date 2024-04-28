5caaa3b7ee4970bf867071701a0589d08de26c3c
Rishabh Munjal <114298525+Rishabh05022005@users.noreply.github.com>
2024-04-28 15:59:44 +0530

    card.js fix

--- static/js/Card.js
+++ static/js/Card.js
@@ -1,17 +1,18 @@
+
 var errors = 100;
 var cardList = [
     "card1",
     "card2"
-];
+]
+
 
 var cardSet;
 var board = [];
 var rows = 4;
-var columns = 3;
+var columns =3;
 
 var card1Selected;
 var card2Selected;
-var staticPath = '/static/img/'; 
 
 window.onload = function() {
     shuffleCards();
@@ -19,77 +20,90 @@ window.onload = function() {
 }
 
 function shuffleCards() {
-    cardSet = cardList.concat(cardList); // Two of each card
-    // Shuffle
+    cardSet = cardList.concat(cardList); //two of each card
+    console.log(cardSet);
+    //shuffle
     for (let i = 0; i < cardSet.length; i++) {
-        let j = Math.floor(Math.random() * cardSet.length); // Get random index
-        // Swap
+        let j = Math.floor(Math.random() * cardSet.length); //get random index
+        //swap
         let temp = cardSet[i];
         cardSet[i] = cardSet[j];
         cardSet[j] = temp;
     }
+    console.log(cardSet);
 }
 
 function startGame() {
+
+
     document.getElementById("board").innerHTML = '';
     
-    // Arrange the board
+    let row = [];
     for (let r = 0; r < rows; r++) {
-        let row = [];
+        
         for (let c = 0; c < columns; c++) {
             let cardImg = cardSet.pop();
-            if (!cardImg) {
-                console.error('Card image is undefined, check the cardSet array');
-                continue; // Skip this iteration and continue with the loop
-            }
-            row.push(cardImg);
+            row.push(cardImg); //JS
 
             let card = document.createElement("img");
             card.id = r.toString() + "-" + c.toString();
-            card.src = staticPath + cardImg + ".png"; // Add the static path to the image source
+            card.src = cardImg + ".png";
             card.classList.add("card");
             card.addEventListener("click", selectCard);
             document.getElementById("board").append(card);
+
         }
         board.push(row);
     }
-    setTimeout(hideCards, 1000); // Adjusted time to ensure images are loaded
+
+    console.log(board);
+    setTimeout(hideCards, 1);
 }
 
 function hideCards() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             let card = document.getElementById(r.toString() + "-" + c.toString());
-            card.src = staticPath + "back.png"; // Add the static path to the back image
+            card.src = "back.png";
         }
     }
 }
 
 function selectCard() {
-    if (this.src.includes("back")) {
-        let coords = this.id.split("-");
-        let r = parseInt(coords[0]);
-        let c = parseInt(coords[1]);
-        let selectedImage = board[r][c];
 
+    if (this.src.includes("back")) {
         if (!card1Selected) {
             card1Selected = this;
-            card1Selected.src = staticPath + selectedImage + ".png";
-        } else if (!card2Selected && this != card1Selected) {
+
+            let coords = card1Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card1Selected.src = board[r][c] + ".png";
+        }
+        else if (!card2Selected && this != card1Selected) {
             card2Selected = this;
-            card2Selected.src = staticPath + selectedImage + ".png";
+
+            let coords = card2Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card2Selected.src = board[r][c] + ".png";
             setTimeout(update, 1000);
         }
     }
+
 }
 
 function update() {
+    //if cards aren't the same, flip both back
     if (card1Selected.src != card2Selected.src) {
-        card1Selected.src = staticPath + "back.png";
-        card2Selected.src = staticPath + "back.png";
-        errors -= 1;
+        card1Selected.src = "back.png";
+        card2Selected.src = "back.png";
+        errors  = errors - 1;
         document.getElementById("errors").innerText = errors;
     }
+
     card1Selected = null;
     card2Selected = null;
 }
